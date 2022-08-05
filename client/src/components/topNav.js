59065a9fa5e5f92d7890373818fdc17fd7aede00
affmde <affmde@hotmail.com>
2022-08-05 10:55:45 +0300

    count up fixed

--- client/src/components/topNav.js
+++ client/src/components/topNav.js
@@ -2,7 +2,6 @@ import { Container, Row, Col, Image } from 'react-bootstrap';
 import { useNavigate } from 'react-router-dom';
 import './topNav.css';
 import CountUp from 'react-countup';
-import { useRef } from 'react';
 
 export const TopNav = (props) => {
 
@@ -41,7 +40,7 @@ export const TopNav = (props) => {
                 {handleInfo().return!=='none' && <Col xs={2} lg={1}><Image alt="return" src="https://img.icons8.com/flat-round/344/thick-long-left-arrow.png" className="return-button" onClick={()=>handleReturn()}></Image></Col>}
                 <Col xs={2} lg={1} className="topNav-text">{handleInfo().title}</Col>
                 <Col xs={{offset: 4, span: 1}} lg={{offset: 6, span: 1}}><Image alt="coins" src="https://img.icons8.com/emoji/2x/coin-emoji.png" className='topNav-coin'></Image></Col>
-                <Col xs={2} lg={1} className="topNav-text">{<CountUp start={props.coins-20} end={props.coins} duration={1}/>}</Col>
+                <Col xs={2} lg={1} className="topNav-text">{<CountUp preserveValue={true} end={props.coins} duration={1}/>}</Col>
             </Row>
         </Container>
     )
